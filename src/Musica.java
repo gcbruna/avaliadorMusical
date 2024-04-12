08aac3026fae11957e2ca9f4f974f60329507602
gcbruna <bruhbg55@gmail.com>
2024-04-12 17:01:35 -0300

    Desafio concluido

---
+++ src/Musica.java
@@ -0,0 +1,7 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        //Crie uma classe Calculadora com um método que recebe um número como parâmetro e retorna o dobro desse número.
+        
+    }
+}
+
